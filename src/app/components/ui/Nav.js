b4a1cb793bb042db7339360c110ea0a65b846bf2
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2023-10-15 10:15:45 +0600

    top nav bar

--- src/app/components/ui/Nav.js
+++ src/app/components/ui/Nav.js
@@ -4,6 +4,10 @@ import { useEffect, useState } from "react";
 import { MdArrowDropDown } from "react-icons/md";
 import { BiSolidUpArrow } from "react-icons/bi";
 import styles from "./Cro.module.css";
+import youtubeIcon from "@/app/components/assets/youtube.png";
+import facebookIcon from "@/app/components/assets/facebook.png";
+import linkedinIcon from "@/app/components/assets/linkedin.png";
+import Image from "next/image";
 
 const Nav = () => {
 	const [isNavbarVisible, setIsNavbarVisible] = useState(true);
@@ -36,9 +40,19 @@ const Nav = () => {
 									}`}
 							>
 								<div className="flex space-x-4 justify-end">
-									<div className="text-white pr-6 cursor-pointer">
-										Youtube
-										{/* <MdArrowDropDown className="inline-block" fontSize={24} /> */}
+									<div className="text-white pr-2 cursor-pointer  transition duration-300 ease-in-out delay-150 hover:scale-105">
+
+										<Image className="inline-block mr-2" src={youtubeIcon} alt="" width={16} height={16} />Youtube
+									</div>
+
+									<div className="text-white pr-2 cursor-pointer  transition duration-300 ease-in-out delay-150 hover:scale-105">
+
+										<Image className="inline-block mr-2" src={facebookIcon} alt="" width={16} height={16} />Facebook
+									</div>
+
+									<div className="text-white pr-2 cursor-pointer  transition duration-300 ease-in-out delay-150 hover:scale-105">
+
+										<Image className="inline-block mr-2" src={linkedinIcon} alt="" width={16} height={16} />Linkedin
 									</div>
 
 									<button className="flex text-gray-800 rounded-full bg-[#dee05d] hover:bg-[#E5E866] px-2 py-0.5 justify-center items-center">
@@ -68,10 +82,6 @@ const Nav = () => {
 
 											</div>
 										)}
-
-
-
-
 									</div>
 
 									<div className="text-white px-3 cursor-pointer">Solutions</div>
@@ -91,12 +101,7 @@ const Nav = () => {
 										Facilities
 										<MdArrowDropDown className="inline-block" fontSize={24} />
 									</div> */}
-
-
-
 								</div>
-
-
 							</div>
 						</div>
 					</div>
